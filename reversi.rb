fc491d9141502190be1c326460f2cf667c08e66a
Yuki Matsukura <matsubokkuri@gmail.com>
2017-03-29 11:48:21 +0000

    sumy feat fujishin

--- reversi.rb
+++ reversi.rb
@@ -1,1 +1,33 @@
-banmen = Array.new(9) { Array.new(9) }
+class Banmen
+  def initialize(yoko, tate)
+    @yoko = yoko
+    @tate = tate
+    @banmen = Array.new(@yoko) { Array.new(@tate) }
+  end
+
+  def oku(yoko, tate, ishi)
+    @banmen[yoko][tate] = ishi
+    reverse
+  end
+
+  def reverse
+  end
+end
+
+class Player
+  def initialize(name, iro)
+    @name = name
+    @iro = iro
+    raise if @iro >= 2  || @iro < 0
+  end
+end
+
+class Game
+  def initialize
+    @banmen = Banmen.new(8, 8)
+    @current_player = Player.new()
+  end
+end
+
+banmen = Banmen.new(8, 8)
+p banmen
